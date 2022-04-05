a45decd9771acf715e5a5c39378d8fc7ab92690c
Nischal Keshav <96449844+NischalKeshav@users.noreply.github.com>
2022-04-05 18:18:16 +0000

    4/5/22 1:17 pm

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import pygame,sys
+from pygame import mixer
 pygame.init()
-
-
-Screen =  pygame.display.set_mode((600,600))
+mixer.init()
+FONT = pygame.font.Font('arial.ttf', 12)
+Screen =  pygame.display.set_mode((710,700))
 BG = (255,255,255)
 clock = pygame.time.Clock()
+pygame.display.set_caption("Europe 20th Century")
 
 
 
+
+mixer.music.load("piano.wav")
+mixer.music.play()
 ###################Images#######################
 Europe = pygame.image.load("Europe.png")
 World = pygame.image.load("world.png")
 ArrowIMG = pygame.image.load("arrow.png")
 tankIMG = pygame.image.load("tank.png")
 stuff = pygame.image.load("Stuff.png")
-ArrowIMG = pygame.transform.rotozoom(ArrowIMG,50,.8)
+ArrowIMG = pygame.transform.rotozoom(ArrowIMG,7,1)
+World = pygame.transform.rotozoom(World,0,1.3)
+
+
 
 
 listOfRects = []
@@ -53,10 +61,35 @@ class Img:
   
     
 #images for animation 1
-Rect1 = Rect(0,0,300,600,7,0) 
-Rect2 = Rect(0,0,600,300,0,7)
-Rect3 = Rect(300,0,300,600,-7,0)
-Rect4  = Rect(0,300,600,300,0,-7)
+Rect4 = Rect(0,0,350,700,10,0) 
+Rect2 = Rect(0,0,700,350,-10,0)
+Rect3  = Rect(350,0,700,350,0,-10)
+Rect1 = Rect(350,0,350,700,0,10)
+
+
+Arrow = Img(ArrowIMG,283,210,rotate=40)
+World = Img(World,150,0) 
+Stuff = Img(stuff,-80,300)
+
+FONT1 = pygame.font.Font("arial.ttf",24)
+
+
+
+a = True
+# Opening animation
+while a:
+  Screen.fill(BG)
+  for i in listOfRects:
+    i.draw()
+  Screen.blit(FONT1.render("Europe in the 20th Century ",4,(255,0,180)),(50,50))
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
+    if event.type == pygame.MOUSEBUTTONDOWN or event.type== pygame.KEYDOWN:
+      a = False
+  pygame.display.update()
+  clock.tick(60)
+
 
 
 
@@ -66,8 +99,12 @@ for i in range(120):
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       sys.exit()
-  
   Screen.blit(Europe,(200,350))
+  Arrow.draw()
+  World.draw()
+  
+  Stuff.draw()
+  
   for i in listOfRects:
     i.move()
     i.draw()
@@ -78,31 +115,51 @@ for i in range(120):
 
 
   
-Arrow = Img(ArrowIMG,300,270,rotate=90)
-World = Img(World,150,80) 
-Stuff = Img(stuff,-100,100)
+
 
 list = []
 
 
 
 
-for i in range(1000):
-  Screen.fill(BG)
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      sys.exit()
+caption1 = """  In the 1900s Europe had incredible 
+influence on almost every inch 
+of the world. Whenever a Major
+event occured in Europe almost all
+nations were affected, whether directly
+due to Europe's immense  empires or 
+indirectly due to trade, allyship, 
+or other forms of movement.
 
-  Screen.blit(Europe,(200,350))
-  Arrow.draw()
-  World.draw()
+"""
+caption2 = """  For example, In both World Wars people, ideas, and 
+resources(espicially weapons) were moved around.
+With the end of the Second  World War  there 
+was a large change in goverments with
+democracy and the fall of  Europe's Empire. 
+"""
+
+
+def render_multi_line(text, x, y, fsize):
+  global FONT      
+  hecolor = (0,0,0)
+  lines = text.splitlines()
+  for i, l in enumerate(lines):
+            Screen.blit(FONT.render(l, 0.0001, hecolor), (x, y + (fsize)*i))
+
+
+for i in range(len(caption1)):
   
-  Stuff.draw()
-  pygame.display.update()
+  Screen.fill((255,255,255))
+  World.draw()
+  render_multi_line(caption1[:i],10,100,18)
+  
+  pygame.display.update(10,100,210,200)
+  clock.tick(60)
+for i in range(len(caption1)):
+  Screen.fill((255,255,255))
+  render_multi_line(caption2[:i],200,520,18)
+  pygame.display.update(200,510,400,700)
   clock.tick(60)
 
-
-#sys.exit()
-  
-  
-  
+#sys.exit()  
